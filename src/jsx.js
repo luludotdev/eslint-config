aa4aabce78e6c89db96bc7dfcae76ee021eb8fbb
Jack Baron <jackmbaron@gmail.com>
2022-10-13 14:33:45 +0100

    extra react rules

--- src/jsx.js
+++ src/jsx.js
@@ -1,5 +1,22 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['neon/jsx'],
-  rules: {},
+  rules: {
+    'react/jsx-fragments': ['error', 'syntax'],
+    'react/jsx-key': [
+      'error',
+      {
+        checkFragmentShorthand: true,
+        checkKeyMustBeforeSpread: true,
+        warnOnDuplicates: true,
+      },
+    ],
+    'react/jsx-no-target-blank': [
+      'error',
+      {
+        forms: true,
+        warnOnSpreadAttributes: true,
+      },
+    ],
+  },
 }
--- src/react.js
+++ src/react.js
@@ -1,5 +1,24 @@
+const jsx = require('./jsx.js')
+
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['neon/react'],
-  rules: {},
+  rules: {
+    ...jsx.rules,
+    'react/boolean-prop-naming': [
+      'error',
+      {
+        validateNested: true,
+      },
+    ],
+    'react/function-component-definition': [
+      'warn',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/no-array-index-key': ['warn'],
+    'react/no-unescaped-entities': ['error'],
+  },
 }
