9a95d0bd0151453671ad9950455fe52645732821
Jack Baron <jackmbaron@gmail.com>
2023-01-13 18:14:35 +0000

    disable some more next.js specific stuff

--- src/next.js
+++ src/next.js
@@ -2,6 +2,12 @@
 module.exports = {
   extends: ['neon/next'],
   rules: {
+    '@typescript-eslint/no-use-before-define': ['off'],
+
+    'n/prefer-global/url-search-params': ['off'],
+
     'react/no-unknown-property': ['error', { ignore: ['jsx', 'global'] }],
+
+    'unicorn/prefer-node-protocol': ['off'],
   },
 }
