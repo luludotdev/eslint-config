d9e095b4ef7a206e9d86468c559443dd35498a86
Jack Baron <jackmbaron@gmail.com>
2023-08-28 18:28:41 +0100

    fix linting

--- scripts/common.mjs
+++ scripts/common.mjs
@@ -1,9 +1,15 @@
 import { readdir } from 'node:fs/promises'
 import { dirname, join } from 'node:path'
+import { argv } from 'node:process'
 import { fileURLToPath } from 'node:url'
 
 const SCRIPTS_DIR = dirname(fileURLToPath(import.meta.url))
 const SRC_DIR = join(SCRIPTS_DIR, '..', 'src')
+
+export const LINT_DIR = join(SCRIPTS_DIR, '..', 'lint')
+export const isLint = () => {
+  return argv.includes('--lint')
+}
 
 export const iterateDir = async () => {
   /**
--- scripts/copy.mjs
+++ scripts/copy.mjs
@@ -1,15 +1,17 @@
 // @ts-check
 
-import { writeFile } from 'node:fs/promises'
-import { parse } from 'node:path'
-import { iterateDir } from './common.mjs'
+import { mkdir, writeFile } from 'node:fs/promises'
+import { join, parse } from 'node:path'
+import { isLint, iterateDir, LINT_DIR } from './common.mjs'
+
+const lint = isLint()
 
 /**
  * @param {string} name
  */
 const legacy = name =>
   `
-const src = require('./src/${name}.js')
+const src = require('${lint ? '..' : '.'}/src/${name}.js')
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
@@ -19,11 +21,14 @@ module.exports = {
 }
 `.trim()
 
+if (lint) await mkdir(LINT_DIR, { recursive: true })
+
 const entries = await iterateDir()
 const jobs = entries.map(async ([base]) => {
   const { name } = parse(base)
+  const path = lint ? join(LINT_DIR, `./${base}`) : `./${base}`
 
-  await writeFile(`./${base}`, legacy(name) + '\n')
+  await writeFile(path, legacy(name) + '\n')
 })
 
 await Promise.all(jobs)
