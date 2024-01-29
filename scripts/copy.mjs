7f06b025efacc1f864b36b86e2eb2d67204aed1a
Jack Baron <jackmbaron@gmail.com>
2024-01-29 07:24:53 +0000

    allow not extending from neon

--- scripts/copy.mjs
+++ scripts/copy.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 
-import { mkdir, writeFile } from 'node:fs/promises'
+import { mkdir, readFile, writeFile } from 'node:fs/promises'
 import { join, parse } from 'node:path'
 import { isLint, iterateDir, LINT_DIR } from './common.mjs'
 
@@ -8,14 +8,15 @@ const lint = isLint()
 
 /**
  * @param {string} name
+ * @param {boolean} extend
  */
-const legacy = name =>
+const legacy = (name, extend = true) =>
   `
 const src = require('${lint ? '..' : '.'}/src/${name}.js')
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: [...src.extends, 'neon/${name}'],
+  extends: [...src.extends${extend ? `, 'neon/${name}'` : ''}],
   plugins: src.plugins(true),
   rules: src.rules,
 }
@@ -24,11 +25,14 @@ module.exports = {
 if (lint) await mkdir(LINT_DIR, { recursive: true })
 
 const entries = await iterateDir()
-const jobs = entries.map(async ([base]) => {
+const jobs = entries.map(async ([base, filepath]) => {
   const { name } = parse(base)
   const path = lint ? join(LINT_DIR, `./${base}`) : `./${base}`
 
-  await writeFile(path, legacy(name) + '\n')
+  const content = await readFile(filepath, 'utf8')
+  const extend = !content.includes('// no-extend\n')
+
+  await writeFile(path, legacy(name, extend) + '\n')
 })
 
 await Promise.all(jobs)
