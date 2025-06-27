81a176065eb8cfd92aced7cf3a49ac8bf372b811
Jack Baron <jackmbaron@gmail.com>
2025-06-27 20:11:42 +0100

    generate exports with tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,4 +4,20 @@ export default defineConfig({
   entry: ["src/*.ts"],
   target: "node22",
   sourcemap: true,
+
+  exports: {
+    customExports: (pkg, _context) => {
+      for (const [id, path] of Object.entries(pkg)) {
+        if (id === "package.json") continue;
+        if (typeof path !== "string") continue;
+
+        pkg[id] = {
+          import: path,
+          types: path.replace(".js", ".d.ts"),
+        };
+      }
+
+      return pkg;
+    },
+  },
 });
